3a84c1165453dd0bacaefdf1da226e62e555a76d
Sam Clay <sam@samclay.uk>
2026-06-24 10:55:26 +0200

    Fix typo in light control for GUEST ENTRANCE toggle

--- server.js
+++ server.js
@@ -194,7 +194,7 @@ app.post("/toggle", (req, res) => {
     sendOSC(turningOn ? "/t/name/aft/1" : "/t/name/aft/0", []);
     log(`OSC: yacht name ${turningOn ? "ON" : "OFF"} (all names)`);
 
-    light["GUEST ENTRANCE"] = turningOn ? "ON" : "OFF";
+    lights["GUEST ENTRANCE"] = turningOn ? "ON" : "OFF";
     sendOSC(turningOn ? "/t/name/guest/1" : "/t/name/guest/0", []);
     log(`OSC: yacht name ${turningOn ? "ON" : "OFF"} (guest entrance)`);
 
